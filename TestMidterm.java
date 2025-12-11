94c477ead80c2f8cd0b71469cac98265b85503af
Chheng Kimter <Chhengkimter@gmail.com>
2025-12-11 09:48:00 +0700

    New logic, more organized

--- TestMidterm.java
+++ TestMidterm.java
@@ -1,71 +1,56 @@
 public class TestMidterm {
     public static void main(String[] args) {
         
-        Adapter one = new twelve(51060);
-        Adapter two = new twentyfour(51060);
+        twelve one = new twelve(51060);
+        twentyfour two = new twentyfour(51060);
 
-        one.convert();
-        ((twelve)one).fortwelve();
-        ((twentyfour)two).fortwentyfour();
+        one.fortwelve();
+        two.fortwentyfour();
 
     }
+    
 }
 
 class Adapter {
-    int sec = 51060;
+    int sec;
     int[] time = new int[3];
 
     Adapter(int sec) {
         this.sec = sec;
+        convert();
     }
 
-    int convert() {
-        time[0] = (sec/60) / 60;
-        time[1] = (sec / 60) % 60;
+    void convert() {
+        time[0] = (sec / 3600);
+        time[1] = (sec % 3600) / 60;
         time[2] = sec % 60;
-        return time[0] * 3600 + time[1] * 60 + time[2];
     }
 }
 
 class twelve extends Adapter {
     twelve(int sec) {
         super(sec);
-        convert();
     }
 
-    int convert() {
-        time[0] = (sec/60) / 60;
-        time[1] = (sec / 60) % 60;
-        time[2] = sec % 60;
-        return time[0] * 3600 + time[1] * 60 + time[2];
-    }
-
-    int fortwelve() {
-        if (time[0] > 12) {
-            time[0] = time[0] - 12;
-            System.out.println(time[0] + ":" + time[1] + ":" + time[2] + " PM");
-        } else {
-            System.out.println(time[0] + ":" + time[1] + ":" + time[2] + " AM");
+    void fortwelve() {
+        int hour = time[0];
+        String period = "AM";
+        
+        if (hour > 12) {
+            hour = hour - 12;
+            period = "PM";
         }
-        return time[0] * 3600 + time[1] * 60 + time[2];
+        
+        System.out.println(hour + ":" + time[1] + ":" + time[2] + " " + period);
     }
 }
 
 class twentyfour extends Adapter {
     twentyfour(int sec) {
         super(sec);
-        convert();
     }
 
-    int convert() {
-        time[0] = (sec/60) / 60;
-        time[1] = (sec / 60) % 60;
-        time[2] = sec % 60;
-        return time[0] * 3600 + time[1] * 60 + time[2];
-    }
-
-    int fortwentyfour() {
+    void fortwentyfour() {
         System.out.println(time[0] + ":" + time[1] + ":" + time[2]);
-        return time[0] * 3600 + time[1] * 60 + time[2];
     }
 }
